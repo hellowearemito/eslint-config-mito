e8e14c61c17649d6c89ee3a16055ce42fafa1514
Kovács Gergely <gg.kovacs@gmail.com>
2015-11-26 08:43:12 +0100

    init es6

--- rules/es6.js
+++ rules/es6.js
@@ -1,6 +1,39 @@
 // ES6
 module.exports = {
+  'env': {
+    'es6': false
+  },
+  'ecmaFeatures': {
+    'arrowFunctions': true,
+    'blockBindings': true,
+    'classes': true,
+    'defaultParams': true,
+    'destructuring': true,
+    'forOf': true,
+    'generators': false,
+    'modules': true,
+    'objectLiteralComputedProperties': true,
+    'objectLiteralDuplicateProperties': false,
+    'objectLiteralShorthandMethods': true,
+    'objectLiteralShorthandProperties': true,
+    'restParams': true,
+    'spread': true,
+    'superInFunctions': true,
+    'templateStrings': true
+  },
   'rules': {
-    
+    'arrow-parens': 0,
+    'arrow-spacing': 0,
+    'constructor-super': 0,
+    'generator-star-spacing': 0,
+    'no-class-assign': 0,
+    'no-const-assign': 2,
+    'no-this-before-super': 0,
+    'no-var': 2,
+    'object-shorthand': 0,
+    'prefer-const': 2,
+    'prefer-spread': 0,
+    'prefer-reflect': 0,
+    'require-yield': 0
   }
 };
